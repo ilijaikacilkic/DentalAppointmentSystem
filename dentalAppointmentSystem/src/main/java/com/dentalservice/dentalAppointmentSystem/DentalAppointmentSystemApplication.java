f2f60eea5e5a420f1b19521360a6a3fec182ea61
ilijaikacilkic <37101107+ilijaikacilkic@users.noreply.github.com>
2021-10-13 18:11:44 +0200

    Initial commit

---
+++ dentalAppointmentSystem/src/main/java/com/dentalservice/dentalAppointmentSystem/DentalAppointmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.dentalservice.dentalAppointmentSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalAppointmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DentalAppointmentSystemApplication.class, args);
+	}
+
+}
---
+++ dentalAppointmentSystem/src/test/java/com/dentalservice/dentalAppointmentSystem/DentalAppointmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dentalservice.dentalAppointmentSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalAppointmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
